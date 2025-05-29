0cf167e740fcb35ed7b521e3f758ec353db529ca
tal.hindi <talhindix@gmail.com>
2025-05-29 16:18:59 +0300

    Exercise 1

---
+++ c/ws1/ex3.c
@@ -0,0 +1,14 @@
+#include <stdio.h> /* printf *\
+
+/***************************
+author:Tal Hindi
+reviewer:Baruchi
+status:approved
+****************************/
+
+int main() {
+    printf("\x22\x48\x65\x6C\x6C\x6F\x20\x57\x6F\x72\x6C\x64\x21\x22\n");
+    return 0;
+}
+
+
