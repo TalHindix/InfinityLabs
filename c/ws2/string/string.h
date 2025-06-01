1064855e8c534e1675d00f1f6fcb9e2aee582cee
tal.hindi <talhindix@gmail.com>
2025-06-01 05:26:28 +0000

    Update string.h

--- c/ws2/string/string.h
+++ c/ws2/string/string.h
@@ -4,6 +4,13 @@
 #include <stddef.h> /* size_t */
 #include <assert.h> /* assert */
 
+/******************************
+Author: Tal Hindi
+Reviewer:
+Status:
+******************************/
+
+
 int StrLen(const char *str);
 int StrCmp(const char *s1, const char *s2);
 
