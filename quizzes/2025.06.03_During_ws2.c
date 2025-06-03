b6d63f20cc01b136b1b144d6ce3ed1b93fac9028
tal.hindi <talhindix@gmail.com>
2025-06-03 16:12:08 +0300

    upload quiz_ws2

---
+++ quizzes/2025.06.03_During_ws2.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+
+
+
+void TF(int num)
+{
+    int i;
+    for (i = 1; i < num; i++)
+    {
+        if (0 == i % 5)
+        {
+            printf("num:%d = %c\n", i, 'T');
+        }
+        else if (0 == i % 3)
+        {
+            printf("num:%d = %c\n", i, 'F');
+        }
+        else if (0 == i % 5 && 0 == i % 3)
+        {
+            printf("num:%d = %s\n", i, "TF");
+        }
+        else
+        {
+            printf("num:%d\n", i);
+        }
+    }
+}
+
+void ReverseString(char *s)
+{
+    char *l;
+    char *r;
+    char temp;
+
+    l = s;
+    r = s+ strlen(s) - 1;
+
+    while (l <= r)
+    {
+        if (*l >= 'A' && *l <= 'Z')
+        {
+            *l += 32;
+        }
+        if (*r >= 'A' && *r <= 'Z')
+        {
+            *r += 32;
+        }
+
+        temp = *l;
+        *l = *r;
+        *r = temp;
+
+        l++;
+        r--;
+    }
+}
+
+
+
+int main()
+{
+	char c[] = "Hello worlD";
+	size_t i;
+	/*Q1*/
+	TF(15);
+	
+	/*Q2*/
+	ReverseString(c);
+	for(i = 0; i < strlen(c); i++)
+  	{
+    printf("%c",*(c+i));
+ 	}
+  	printf("\n");
+  	
+	
+	
+
+	return 1;
+}
