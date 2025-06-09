b1e651aa9a9b438c1de439ebb73ecd429bfed7e0
tal.hindi <talhindix@gmail.com>
2025-06-09 15:05:15 +0300

    uploaded ws5 ex1.c

---
+++ c/ws5/ex1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+
+void Print(int value)
+{
+	printf("%d\n",value);
+}
+
+
+struct print_me
+{
+	int myNum;
+	void (*print_func)(int); 
+};
+
+
+
+int main()
+{
+	struct print_me arr[10];
+	size_t i = 0;
+	
+	for (i = 0; i < 10 ; i++)
+	{
+		arr[i].myNum = i * 2;
+		arr[i].print_func = Print;
+		arr[i].print_func(arr[i].myNum);
+	}
+	
+	return 0;
+}
