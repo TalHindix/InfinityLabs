28b67f1f0a0262f367d9e956a949b405b602fcaa
tal.hindi <talhindix@gmail.com>
2025-11-25 16:29:55 +0200

    Upload BitArray files ~

---
+++ cpp/inc/bitarray.hpp
@@ -0,0 +1,20 @@
+/*****************************************************************************
+ * Exercise:    BitArray
+ * Date:        25/11/2025
+ * Developer:   Tal Hindi
+ * Reviewer:
+ * Status:
+ *****************************************************************************/
+
+#ifndef __ILRD_BITARRAY__
+#define __ILRD_BITARRAY__
+
+#include <iostream> // std::ostream
+
+namespace ilrd
+{
+    
+}
+
+
+#endif /* __ILRD_BITARRAY__ */
